a263e77f3f5ca16046b955e447f8b8fbe5f2ccba
JRazek <jakub.razek2@gmail.com>
2024-02-10 21:00:48 +0100

    bars

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ use plotters::prelude::*;
 fn main() {
     let drawing_area = SVGBackend::new("./lin_reg.svg", (800, 600)).into_drawing_area();
 
-    reg_lin::run(ChartBuilder::on(&drawing_area));
+    let (left, right) = drawing_area.split_horizontally(400);
+
+    reg_lin::run(ChartBuilder::on(&left), ChartBuilder::on(&right));
 
     drawing_area.present().unwrap();
 }
--- src/reg_lin.rs
+++ src/reg_lin.rs
@@ -5,7 +5,9 @@ use rand::distributions::Standard;
 use rand::prelude::*;
 use rand::rngs::OsRng;
 
-pub fn run<B>(mut chart_builder: ChartBuilder<B>)
+pub fn regression() {}
+
+pub fn run<B>(mut line_plot: ChartBuilder<B>, mut error_histogram_plot: ChartBuilder<B>)
 where
     B: DrawingBackend,
 {
@@ -18,7 +20,7 @@ where
     let y_modeled: Array<_, _> = x.iter().map(|&x| theta0 + x * theta1).collect();
     let y = y_modeled
         .iter()
-        .map(|&y| y + OsRng.sample::<f64, _>(Standard))
+        .map(|&y| y + (OsRng.sample::<f64, _>(Standard)) - 0.5)
         .collect::<Vec<_>>();
 
     let line_series = LineSeries::new(
@@ -30,46 +32,67 @@ where
     )
     .point_size(1);
 
-    let mut chart_context = chart_builder
-        .caption("function, lmao", &BLACK)
+    let mut line_context = line_plot
+        .caption("function", &BLACK)
         .set_all_label_area_size(50)
         .margin(10)
         .build_cartesian_2d(0f64..10f64, 0f64..32f64)
         .unwrap();
 
-    chart_context
+    line_context
         .configure_mesh()
         .x_labels(20)
         .y_labels(10)
         .draw()
         .unwrap();
 
-    chart_context
+    line_context
         .draw_series(line_series)
         .unwrap()
         .label("modeled")
         .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], BLACK));
 
-    chart_context
+    line_context
         .draw_series(
             x.iter()
                 .cloned()
-                .zip(y)
+                .zip(y.clone())
                 .map(|(x, y)| Circle::new((x, y), 2, RED.filled())),
         )
         .unwrap()
         .label("data")
         .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], RED));
 
-    //    chart_context
-    //        .draw_series(
-    //            Histogram::vertical(&chart_context)
-    //                .style(RED.mix(0.5).filled())
-    //                .data(y.iter().map(|&x| (x, 1))),
-    //        )
-    //        .unwrap();
+    let mut histogram_context = error_histogram_plot
+        .caption("error: |y - y^|", &BLACK)
+        .set_all_label_area_size(50)
+        .margin(10)
+        .build_cartesian_2d(0usize..y_modeled.len(), 0f64..1f64)
+        .unwrap();
 
-    chart_context
+    histogram_context
+        .configure_mesh()
+        .x_labels(20)
+        .y_labels(10)
+        .draw()
+        .unwrap();
+
+    histogram_context
+        .draw_series(
+            Histogram::vertical(&histogram_context)
+                .style(RED.mix(0.5).filled())
+                .margin(0) //distance between bars
+                .data(
+                    y.iter()
+                        .cloned()
+                        .zip(y_modeled)
+                        .enumerate()
+                        .map(|(i, (y, y_modeled))| (i, (y - y_modeled).abs())),
+                ),
+        )
+        .unwrap();
+
+    line_context
         .configure_series_labels()
         .border_style(BLACK)
         .background_style(WHITE)
