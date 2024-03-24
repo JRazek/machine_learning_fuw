ceaac5219772656f3f8f4e50d98dd969d2a74a87
JRazek <jakub.razek2@gmail.com>
2024-03-24 17:35:55 +0100

    poly fit sketch

--- src/bin/poly_fit.rs
+++ src/bin/poly_fit.rs
@@ -1,25 +1,45 @@
+use std::array::from_fn;
+
 use dfdx::prelude::*;
+use rand::rngs::ThreadRng;
 use rand_distr::Distribution;
+use rand_distr::Uniform;
+
+fn poly(x: f32) -> f32 {
+    2. * x.powi(2) * 3. * x - 2.
+}
 
 fn generate_dataset<const N: usize, D>(dev: D) -> Tensor<Rank2<N, 2>, f32, D, NoneTape>
 where
     D: Device<f32>,
 {
-    let rng = rand::thread_rng();
-    let dist = rand_distr::Uniform::new(0., 1.).sample_iter(rng);
-    let data: Vec<f32> = dist
-        .take(N)
-        .map(|x| [x, if x < 0.5 { 0. } else { 1. }])
-        .flatten()
-        .collect();
+    let rng = ThreadRng::default();
+    let uniform = Uniform::new(-1., 1.);
 
-    dev.tensor_from_vec(data, Rank2::<N, 2>::default())
+    let dataset = uniform
+        .sample_iter(rng)
+        .take(N)
+        .map(|x| {
+            let y = poly(x);
+            [x, y]
+        })
+        .flatten()
+        .collect::<Vec<_>>();
+
+    let tensor: Tensor<Rank2<N, 2>, _, _, _> =
+        dev.tensor_from_vec(dataset, (Const::<N>, Const::<2>));
+
+    tensor
 }
 
 fn draw_dataset<const N: usize, D>(tensor: Tensor<Rank2<N, 2>, f32, D>)
 where
     D: Device<f32>,
 {
+}
+
+struct NeuronModel {
+//    linear: LinearConstConfig
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
