e5473d44d83f0f4772218df2dab063830ac5fc7e
JRazek <jakub.razek2@gmail.com>
2024-06-01 19:01:02 +0200

    preprocess vocabulary

--- src/bin/data_streams.rs
+++ src/bin/data_streams.rs
@@ -11,6 +11,9 @@ use std::borrow::Cow;
 struct Args {
     #[clap(short, long, default_value = "datasets/korpus/")]
     dataset_path: String,
+
+    #[clap(short, long, default_value = "datasets/dictionary.txt")]
+    dictionary_path: String,
 }
 
 fn filter_wksf_dataset<'a>(dataset: &'a str) -> Cow<'a, str> {
@@ -22,18 +25,66 @@ fn filter_wksf_dataset<'a>(dataset: &'a str) -> Cow<'a, str> {
     dataset
 }
 
+fn build_dictionary<'a>(datasets: impl Iterator<Item = &'a str>) -> Vec<&'a str> {
+    let mut dictionary = datasets.collect::<Vec<&str>>();
+
+    dictionary.sort();
+    dictionary.dedup();
+
+    dictionary
+}
+
+fn prepare_dictionary<'a>(
+    dictionary_path: &str,
+    datasets: impl Iterator<Item = &'a str>,
+) -> Result<Vec<(usize, String)>, std::io::Error> {
+    let dictionary: Vec<(usize, String)> = match read_to_string(dictionary_path) {
+        Ok(dictionary) => {
+            println!("cached dictionary exists.");
+
+            dictionary
+                .lines()
+                .map(|x| x.to_string())
+                .enumerate()
+                .collect()
+        }
+        Err(_) => {
+            println!("dictionary not found. Building dictionary..");
+
+            let dictionary = build_dictionary(datasets);
+
+            std::fs::write(dictionary_path, dictionary.join("\n"))?;
+
+            dictionary
+                .iter()
+                .map(|x| x.to_string())
+                .enumerate()
+                .collect()
+        }
+    };
+
+    Ok(dictionary)
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let Args { dataset_path }: Args = Args::parse();
+    let Args {
+        dictionary_path,
+        dataset_path,
+    }: Args = Args::parse();
 
     println!("reading files from {}..", dataset_path);
 
     let files = read_dir(dataset_path)?;
 
+    let mut datasets = Vec::new();
+
     for file in files {
         let content = read_to_string(file?.path())?;
         let filtered_dataset = filter_wksf_dataset(&content);
-        println!("{}", filtered_dataset);
+        datasets.push(filtered_dataset.into_owned());
     }
+
+    let dictionary = prepare_dictionary(&dictionary_path, datasets.iter().map(|x| x.as_str()))?;
 
     Ok(())
 }
