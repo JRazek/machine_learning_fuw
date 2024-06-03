797b27a1dfbd6aafc25ebfa22ea48edb8ec770f9
JRazek <jakub.razek2@gmail.com>
2024-06-04 00:16:51 +0200

    added embeddings training

--- src/bin/word2vec.rs
+++ src/bin/word2vec.rs
@@ -1,41 +1,146 @@
+#![feature(iter_repeat_n)]
+#![feature(generic_const_exprs)]
+
 use clap::Parser;
 use std::fs::read_dir;
 use std::fs::read_to_string;
 
+use std::collections::HashMap;
+
 use uczenie_maszynowe_fuw::data_streams::*;
+
+use dfdx::data::OneHotEncode;
+use dfdx::prelude::*;
+
+#[derive(Sequential, Default, Debug, Clone)]
+struct EmbeddingNet<const VOCAB_SIZE: usize, const EMBEDDING_SIZE: usize> {
+    embedding: LinearConstConfig<VOCAB_SIZE, EMBEDDING_SIZE>,
+}
+
+fn embeddings_training<
+    const DICTIONARY_SIZE: usize,
+    const EMBEDDING_SIZE: usize,
+    const RADIUS: usize,
+    E,
+    D,
+    M,
+>(
+    dataset: Vec<u32>,
+    dev: D,
+    embedding_net: M,
+    adam_config: AdamConfig,
+) -> Result<(), Box<dyn std::error::Error>>
+where
+    E: Dtype + rand::distributions::uniform::SampleUniform + num::Float + num::FromPrimitive,
+    D: Device<E>,
+    [E; 2 * RADIUS]:,
+    M: Module<
+            Tensor<Rank2<{ 2 * RADIUS }, DICTIONARY_SIZE>, E, D, OwnedTape<E, D>>,
+            Output = Tensor<Rank2<{ 2 * RADIUS }, EMBEDDING_SIZE>, E, D, OwnedTape<E, D>>,
+        > + UpdateParams<E, D>,
+{
+    let output_layer = LinearConstConfig::<EMBEDDING_SIZE, DICTIONARY_SIZE>::default();
+    let output_layer_module = output_layer.build_on_device(&dev);
+
+    let stop_iter = std::iter::repeat_n(0, RADIUS); //stop words padding
+
+    let dataset: Vec<u32> = stop_iter
+        .clone()
+        .chain(dataset.into_iter())
+        .chain(stop_iter)
+        .collect();
+
+    let mut module = (embedding_net, output_layer_module);
+
+    let mut optimizer = Adam::new(&module, adam_config);
+
+    println!("training..");
+
+    for window in dataset.repeat(1).windows(2 * RADIUS + 1) {
+        let current_token = window[RADIUS] as usize;
+        let neighbours: [usize; 2 * RADIUS] = window[0..RADIUS]
+            .iter()
+            .chain(window[RADIUS + 1..].iter())
+            .map(|&x| x as usize)
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap();
+
+        println!("neighbours: {:?}", neighbours);
+
+        let input: Tensor<Rank2<{ 2 * RADIUS }, DICTIONARY_SIZE>, E, D, OwnedTape<_, _>> = dev
+            .one_hot_encode(Const::<DICTIONARY_SIZE>, [current_token; 2 * RADIUS])
+            .retaped();
+
+        let target: Tensor<Rank2<{ 2 * RADIUS }, DICTIONARY_SIZE>, E, D> =
+            dev.one_hot_encode(Const::<DICTIONARY_SIZE>, neighbours);
+
+        let output = module.forward(input);
+
+        let loss = cross_entropy_with_logits_loss(output, target);
+
+        println!("loss: {:?}", loss.as_vec()[0]);
+
+        let grads = loss.backward();
+
+        optimizer.update(&mut module, &grads)?;
+    }
+
+    Ok(())
+}
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[clap(short, long, default_value = "datasets/korpus/")]
     dataset_path: String,
-
-    #[clap(short, long, default_value = "datasets/dictionary.txt")]
-    dictionary_path: String,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let Args {
-        dictionary_path,
-        dataset_path,
-    }: Args = Args::parse();
+    let Args { dataset_path }: Args = Args::parse();
 
     println!("reading files from {}..", dataset_path);
 
     let files = read_dir(dataset_path)?;
 
-    let mut datasets = Vec::new();
+    let mut dataset: Vec<String> = Vec::new();
 
     for file in files {
         let content = read_to_string(file?.path())?;
-        let filtered_dataset = filter_wksf_dataset(&content);
-        datasets.push(filtered_dataset.into_owned());
+        let mut filtered_dataset =
+            preprocess_raw_text(&filter_wksf_dataset(&content)).collect::<Vec<String>>();
+
+        dataset.append(&mut filtered_dataset);
     }
 
-    let dictionary = prepare_dictionary(&dictionary_path, datasets.iter().map(|x| x.as_str()))?;
+    let dictionary = build_dictionary(dataset.iter().map(|x| x.as_str()));
 
-    println!("dictionary prepared, size: {}", dictionary.len());
-    println!("{:?}", dictionary);
+    //    println!("dataset: {:?}", dataset);
+    println!("dictionary length: {:?}", dictionary.len());
+
+    const DICTIONARY_SIZE: usize = 85379;
+    assert_eq!(dictionary.len(), DICTIONARY_SIZE);
+
+    //    let inverse_dictionary = inverse_dictionary(&dictionary);
+
+    println!("tokenizing dataset..");
+
+    let tokenized_datasets_flattened: Vec<u32> =
+        tokenize_preprocessed_text(dataset.iter().map(|x| x.as_str()), &dictionary);
+
+    println!("tokenized dataset successfully",);
+
+    let dev = AutoDevice::default();
+
+    let adam_config = AdamConfig::default();
+
+    let module = dev.build_module::<f32>(EmbeddingNet::default());
+    embeddings_training::<DICTIONARY_SIZE, 1000, 2, f32, _, _>(
+        tokenized_datasets_flattened,
+        dev,
+        module,
+        adam_config,
+    )?;
 
     //    let tokenizer = tokenizers::Tokenizer::new(dictionary);
 
--- src/data_streams.rs
+++ src/data_streams.rs
@@ -9,57 +9,46 @@ pub fn filter_wksf_dataset<'a>(dataset: &'a str) -> Cow<'a, str> {
         regex::Regex::new(r"((\d+~[^~]+.*)|(\[(\d+|\/|\+|\#|\?|\&|\~|\,\,|\=|\$)])|[^\w\s])")
             .unwrap();
 
-    let dataset = citations_reg.replace_all(&dataset, "");
+    let dataset = citations_reg.replace_all(&dataset, " ");
 
     dataset
 }
 
-pub fn build_dictionary<'a>(datasets: impl Iterator<Item = &'a str>) -> HashMap<u32, String> {
+pub fn preprocess_raw_text(dataset: &str) -> impl Iterator<Item = String> + '_ {
+    let ds = dataset.split_whitespace().map(|x| x.to_lowercase());
+
+    ds
+}
+
+pub fn build_dictionary<'a>(
+    preprocessed_datasets: impl Iterator<Item = &'a str>,
+) -> HashMap<String, u32> {
+    let mut dictionary = preprocessed_datasets
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>();
+
+    dictionary.sort();
+    dictionary.dedup();
+
     let dictionary = std::iter::once("<stop>".to_string())
-        .chain(
-            datasets
-                .flat_map(|x| x.split_whitespace())
-                .map(|x| x.to_lowercase()),
-        )
+        .chain(dictionary)
+        .into_iter()
         .enumerate()
-        .map(|(i, x)| (i as u32, x))
+        .map(|(i, x)| (x, i as u32))
         .collect();
 
     dictionary
 }
 
-pub fn prepare_dictionary<'a>(
-    dictionary_path: &str,
-    datasets: impl Iterator<Item = &'a str>,
-) -> Result<HashMap<u32, String>, std::io::Error> {
-    let dictionary: HashMap<u32, String> = match read_to_string(dictionary_path) {
-        Ok(dictionary) => {
-            println!("cached dictionary exists.");
-
-            dictionary
-                .lines()
-                .map(|x| x.to_string())
-                .enumerate()
-                .map(|(i, x)| (i as u32, x))
-                .collect()
-        }
-        Err(_) => {
-            println!("dictionary not found. Building dictionary..");
-
-            let dictionary = build_dictionary(datasets);
-
-            std::fs::write(
-                dictionary_path,
-                dictionary
-                    .values()
-                    .map(|x| x.as_str())
-                    .collect::<Vec<_>>()
-                    .join("\n"),
-            )?;
-
-            dictionary
-        }
-    };
-
-    Ok(dictionary)
+pub fn tokenize_preprocessed_text<'a>(
+    dataset: impl Iterator<Item = &'a str>,
+    dictionary: &HashMap<String, u32>,
+) -> Vec<u32> {
+    dataset
+        .map(|x| dictionary.get(x).unwrap_or(&0).clone())
+        .collect()
 }
+
+//pub fn inverse_dictionary(dictionary: &HashMap<u32, String>) -> HashMap<String, u32> {
+//    dictionary.iter().map(|(x, y)| (y.clone(), *x)).collect()
+//}
